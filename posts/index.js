991deb3e706ade7fb7e1c4610e11f61aab566b94
imgss <838632526@qq.com>
2017-05-29 12:30:33 +0800

    生成文章索引

---
+++ posts/index.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+ function promiseFile(path){
+	 return new Promise(function(resolve,reject){
+		fs.readFile(path,'utf-8',(err,data)=>{
+			if(err){
+				reject();
+				throw err;
+			}
+			let re = /---\r\n([\s\S]+)\r\n---([\s\S]+)/gm;
+			let match = re.exec(data);
+				if(match){
+					let info = match[1];
+					let [title, tags, date] = info.split('\r\n')
+					let article = match[2];
+					resolve({
+						title,
+						tags,
+						date,
+						text:article.substr(0,200).replace(/\r|\n|#/g, ' ')
+					})
+				}
+		})
+	 })
+ }
+function walk(path){
+	let articleList = [];
+	let promiseArr = [];
+	var dirList = fs.readdirSync(path);
+	console.log(dirList)
+	dirList.forEach(function(item){
+		if(fs.statSync(path + '/' + item).isDirectory()){
+			walk(path);
+		}else{
+			if(/\.md/.test(item)){
+				promiseArr.push(promiseFile(path + '/' + item))
+			}
+		}
+	});
+	Promise.all(promiseArr)
+		   .then(values => fs.writeFile(__dirname+'/index.json', JSON.stringify(values), (err) => {
+			   if(err){
+			   		throw (err)
+				}
+			   console.log(done)
+			}))
+}
+console.log(__dirname)
+walk(__dirname);
