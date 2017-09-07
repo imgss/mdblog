a6af47fcd2e50d9d05239d2ee201413feaf7e723
imgss <838632526@qq.com>
2017-09-07 22:47:11 +0800

    更新博客

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
-var index = require('./posts/index.js')
-console.log(index)
-index(__dirname+'/posts')
+var fs = require('fs');
+let allTags = [];
+ function promiseFile(path){
+	 return new Promise(function(resolve,reject){
+		fs.readFile(path, 'utf-8', (err,data) => {
+			if(err){
+				reject();
+				throw err;
+			}
+			data = data.replace(/\r/g,'');
+			let re = /---\n([\s\S]+)\n---([\s\S]+)/gm;
+			let match = re.exec(data);
+				if(match){
+					let info = match[1];
+					let [title, tags, postDate] = info.split(/\n/)
+					let article = match[2];
+					console.log(title)
+					try{
+					title = title.split(':')[1].trim();
+					tags = tags.split(':')[1].trim();	
+					postDate = postDate.replace(/date\s?:\s+/, '')	
+					allTags.push(tags);
+					resolve({
+						title,
+						tags,
+						postDate,
+						id: '/article/' + path.split('/').pop().split('.')[0],
+						text: article.substr(0, 200).replace(/\r|\n|#/g, '').replace(title, '')
+					})
+					}catch(err){
+						console.log('err',info)
+						let [ti, tag, po] = info.split(/\n/);
+					}
+				}else{
+					resolve();
+					console.log(path,'没有标签')
+				}
+		})
+	 })
+ }
+ function walk(path){
+	let articleList = [];
+	let promiseArr = [];
+	var dirList = fs.readdirSync(path);
+	dirList.forEach(function(item){
+		if(fs.statSync(path + '/' + item).isDirectory()){
+			walk(path);
+		}else{
+			if(/\.md/.test(item)){
+				console.log(item)
+				promiseArr.push(promiseFile(path + '/' + item))
+			}
+		}
+	});
+	Promise.all(promiseArr)
+		   .then(values => {
+			   allTags = [...new Set(allTags.join(' ').split(/\s+/))]
+				 values.sort((a,b) => new Date(b.postDate) - new Date(a.postDate))//按日期降序排列
+			   fs.writeFile(__dirname + '/posts/index.json', JSON.stringify({values,allTags},null,2), (err) => {
+			   if(err){
+			   		throw (err)
+				}
+			   console.log('done')
+			})
+		}).catch(err => {
+			throw(err);
+		})
+}
+walk(__dirname + '/posts');
--- new.js
+++ new.js
@@ -1,16 +1,18 @@
 const FS = require('fs');
-const info = `
----
-title: 
-tags: 
-date: ${(new Date).toLocaleString()}
----
-`
+
 let name = process.argv[2] ||`new.md`;
 if(!/\.md/.test(name)){
     name = name + '.md'
 }
-FS.writeFile(__dirname+`/${name}`, info, err =>{
+
+const info = `
+---
+title: ${name}
+tags: 
+date: ${(new Date).toLocaleString()}
+---
+`
+FS.writeFile(__dirname + `/posts/${name}`, info, err =>{
     if(err){
         console.log('error');
     }
